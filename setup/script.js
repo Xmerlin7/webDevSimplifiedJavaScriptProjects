5f0f142f0f5f91fc8046982004721fd72523dbeb
Seif Allah <saifeldarageely@gmail.com>
2024-09-16 15:43:53 +0300

    #

--- setup/script.js
+++ setup/script.js
@@ -1,10 +1,25 @@
-function identy(name, b) {
-    console.log("hello " + name + " u realy " + b + " years old !")
-}
-function func(x) {
-    let a = "youmn"
-    let b = 7
-    x((a, b))
-}
+// function that takes arguments and passes them to the callback
+function processData (dataA, dataB, callback) {
 
-func (identy) 
+
+    // call the callback function with the processed data
+    callback(dataA, dataB);
+}
+function processA (dataA, dataB){
+    // perform some operations on the data
+    let resultA = dataA * 2;
+    let resultB = dataB;
+    // display the results on console
+    console.log('Processed Data:', resultA, resultB);
+}
+function processB (dataA, dataB){
+    // perform some operations on the data
+    let resultA = dataA;
+    let resultB = dataB / 3;
+    // display the results on console
+    console.log('Processed Data:', resultA, resultB);
+}
+dataA = 5
+dataB = 15
+processData(dataA, dataB, processA);
+processData(dataA, dataB, processB);
