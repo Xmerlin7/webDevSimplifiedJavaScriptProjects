1885d9a54f504dcfb3d31eb1a452bf2e653c1311
Seif Allah <saifeldarageely@gmail.com>
2024-09-16 17:34:28 +0300

    Refactor script.js and add new function
    This commit refactors the script.js file by removing unused code and adding a new function called func. The func function takes an argument x and a callback function, and logs the result of calling the callback with x. This change improves the code structure and adds new functionality to the script.

--- setup/script.js
+++ setup/script.js
@@ -1,29 +1,9 @@
-// function that takes arguments and passes them to the callback
-function processData(dataA, dataB, callback) {
-
-
-    // call the callback function with the processed data
-    callback(dataA, dataB);
+/**
+ * Function description here.
+ * 
+ * @function
+ */
+function func(x, callback){
+    console.log(callback(x));
 }
-function processA(dataA, dataB) {
-    // perform some operations on the data
-    let resultA = dataA * 2;
-    let resultB = dataB;
-    // display the results on console
-    console.log('Processed Data:', resultA, resultB);
-}
-function processB(dataA, dataB) {
-    // perform some operations on the data
-    let resultA = dataA;
-    let resultB = dataB / 3;
-    // display the results on console
-    console.log('Processed Data:', resultA, resultB);
-}
-dataA = 5
-dataB = 15
-processData(dataA, dataB, processA);
-processData(dataA, dataB, processB);
-let printName = name => {
-    console.log(name);
-}
-printName('John Doe');
+func(2, fn  => fn + 1 )
