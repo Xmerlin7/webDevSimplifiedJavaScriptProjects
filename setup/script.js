bbc36985f4edaa92af64087ebf87598029e2197d
Seif Allah <saifeldarageely@gmail.com>
2024-09-16 16:18:18 +0300

    #

--- setup/script.js
+++ setup/script.js
@@ -1,18 +1,18 @@
 // function that takes arguments and passes them to the callback
-function processData (dataA, dataB, callback) {
+function processData(dataA, dataB, callback) {
 
 
     // call the callback function with the processed data
     callback(dataA, dataB);
 }
-function processA (dataA, dataB){
+function processA(dataA, dataB) {
     // perform some operations on the data
     let resultA = dataA * 2;
     let resultB = dataB;
     // display the results on console
     console.log('Processed Data:', resultA, resultB);
 }
-function processB (dataA, dataB){
+function processB(dataA, dataB) {
     // perform some operations on the data
     let resultA = dataA;
     let resultB = dataB / 3;
@@ -23,3 +23,7 @@ dataA = 5
 dataB = 15
 processData(dataA, dataB, processA);
 processData(dataA, dataB, processB);
+let printName = name => {
+    console.log(name);
+}
+printName('John Doe');
