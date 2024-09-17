2a101092b88ed1a8e95d579285948d280d159cab
Seif Allah <saifeldarageely@gmail.com>
2024-09-17 18:24:36 +0300

    Refactor script.js and update object and array values

--- setup/script.js
+++ setup/script.js
@@ -1,9 +1,13 @@
-let porch = {
-    make: 2023,
-    model: "Porch",
-    condition: "new",
-    info(){
-        console.log(porch.make + " " + porch.model + " is " + porch.condition);
-    }
+let a = [1, 2, 3];
+let b = a;
+obj = {name: 'John'};
+obj2 = obj;
+changeValue(b, obj2);
+console.log(a); 
+
+
+console.log(obj)
+function changeValue(b, n) {
+    b[0] = 5;
+    n.name = 'Jane';
 }
-porch.info(); // 2023 Porch is new
