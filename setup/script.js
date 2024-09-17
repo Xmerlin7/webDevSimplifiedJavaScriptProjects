9c39bf47a824617398f9bd11d20f365f7361a739
Seif Allah <saifeldarageely@gmail.com>
2024-09-18 01:40:58 +0300

    Refactor script.js to update HTML elements' styles and add window resize event listener

--- setup/script.js
+++ setup/script.js
@@ -5,3 +5,7 @@ const idOfDev = document.getElementById('div-id')
 idOfDev.style.color = 'red'
 const classIds = document.getElementsByClassName('div-class')
 classIds[0].style.color = 'blue'
+
+Array.from(classIds).forEach((Element) => {
+    Element.style.color = 'green'  
+})
