b8c29e6e24e1fe6c48a4b6d04637108b3186ac14
Seif Allah <saifeldarageely@gmail.com>
2024-09-18 01:28:34 +0300

    Refactor script.js to update HTML elements' styles

--- setup/script.js
+++ setup/script.js
@@ -1,3 +1,7 @@
 window.addEventListener('resize', () => {
     console.log('window resized')
 })
+const idOfDev = document.getElementById('div-id')
+idOfDev.style.color = 'red'
+const classIds = document.getElementsByClassName('div-class')
+classIds[0].style.color = 'blue'
