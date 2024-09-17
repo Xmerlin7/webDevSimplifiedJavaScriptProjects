61e358261f7acea482dd9875bb0365be061fa7c0
Seif Allah <saifeldarageely@gmail.com>
2024-09-17 16:23:30 +0300

    Refactor script.js and add new object method

--- setup/script.js
+++ setup/script.js
@@ -1,21 +1,9 @@
-/**
- * @description: This is a function that returns another function.
- * The inner function has access to the outer function's variables.
- * This is called a closure that made me psyco.
- * 
- * @function
- */
-function setup(varaiable) {
-
-    let c = 10
-    return function func(varaiable2) {
-        console.log(varaiable)
-        console.log(varaiable2)
-        console.log(c)
-
+let porch = {
+    make: 2023,
+    model: "Porch",
+    condition: "new",
+    info(){
+        console.log(porch.make + " " + porch.model + " is " + porch.condition);
     }
 }
-let closure = setup(1)
-closure(2)
-const a = [[1, 2, 3, 4, 5], [6, 7, 8, 9, 10]]
-console.log(a[1][2])
+porch.info(); // 2023 Porch is new
