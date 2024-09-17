ef374abe175eedc08b311391a136764ee48f655b
Seif Allah <saifeldarageely@gmail.com>
2024-09-18 00:57:42 +0300

    Refactor script.js to remove unused code and add window resize event listener

--- setup/script.js
+++ setup/script.js
@@ -1,11 +1,3 @@
-const items = [
-    {price: 10},
-    {price: 20},
-    {price: 30},
-    {price: 40},
-    {price: 50}
-];
-const total = items.reduce((sum, item) => {
-    return sum + item.price
-}, 0);
-console.log(total);
+window.addEventListener('resize', () => {
+    console.log('window resized')
+})
