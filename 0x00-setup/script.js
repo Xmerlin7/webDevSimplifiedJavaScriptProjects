9a238d2f6738db6db2d95a9c0fca96039dc6ede6
Seif Allah <saifeldarageely@gmail.com>
2024-09-19 00:20:25 +0300

    Refactor script.js to remove unnecessary code and add array iteration

--- 0x00-setup/script.js
+++ 0x00-setup/script.js
@@ -1,10 +1,4 @@
-const grandParent = document.querySelector('#grandparent');
-// const parent = document.querySelector('#parent');
-const child = document.querySelector('#child');
-grandParent.style.color = 'red';
-const parent1 = grandParent.children[0]
-const parent2 = grandParent.children[1]
-
-parent1.style.color = 'blue';
-parent2.children[0].style.color = 'blue';
-
+const arr = [1, 2, 3, 4, 5];
+arr.forEach(item => {
+    console.log(item);
+})
