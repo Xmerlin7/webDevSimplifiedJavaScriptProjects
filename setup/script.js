b31096c4d0731f1b9c98c8266542b1c2c64f8cee
Seif Allah <saifeldarageely@gmail.com>
2024-09-18 04:01:16 +0300

    Refactor script.js to update HTML elements' styles and add button click event listener

--- setup/script.js
+++ setup/script.js
@@ -1,12 +1,6 @@
-window.addEventListener('resize', () => {
-    console.log('window resized')
-})
-const idOfDev = document.getElementById('div-id')
-idOfDev.style.color = 'red'
-const classIds = document.getElementsByClassName('div-class')
-
-
-Array.from(classIds).forEach((Element) => {
-    Element.style.color = 'green'  
-})
-classIds[0].style.color = 'blue'
+const btn = document.querySelector('[data-btn]')
+function clicked(){
+    console.log('Button Clicked')
+}
+btn.addEventListener('click', clicked)
+btn.addEventListener('mouseenter', clicked)
