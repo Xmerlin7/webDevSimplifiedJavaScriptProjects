388244d5c3b43ec55acaea9669c86a817f48ce96
Seif Allah <saifeldarageely@gmail.com>
2024-09-18 21:07:45 +0300

    Refactor script.js to add form submission functionality and improve task list interaction

--- 0x01-toDoList/script.js
+++ 0x01-toDoList/script.js
@@ -7,11 +7,16 @@ const list = document.querySelector('#list')
 form.addEventListener('submit', e => {
     e.preventDefault()
     //?create Item which inserted in input
-
-    //? add a new Item to the list
+    const item = document.createElement('div')
+    item.innerText = input.value
+    
+        //? add a new Item to the list
+    item.classList.add('list-item') 
 
     //? clear the input 
-
+    input.value = ''
     //? remove the task if finished
-    
+    item.addEventListener('click', () => {
+        item.remove()
+    })
 })
