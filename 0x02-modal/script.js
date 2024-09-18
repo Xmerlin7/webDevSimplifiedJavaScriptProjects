f2035c3a098a8a4996159b1f398b31c72c33d435
Seif Allah <saifeldarageely@gmail.com>
2024-09-18 22:04:13 +0300

    Refactor script.js to add open and close functionality for modal and overlay

--- 0x02-modal/script.js
+++ 0x02-modal/script.js
@@ -11,22 +11,18 @@ const closeModalBtn = document.getElementById('close-modal-btn');
 const overlay = document.getElementById('overlay');
 
 // TODO: 3. Create a click event listener for the open-modal-btn that adds the class "open" to the modal
-openModalBtn.addEventListener('click', () => {
-    modal.classList.add('open');
-    // BONUS: Also add the class "open" to the overlay
-    overlay.classList.add('open');
-})
+openModalBtn.addEventListener('click', openModal);
 
 // TODO: 4. Create a click event listener for the close-modal-btn that removes the class "open" from the modal
 // BONUS: Also remove the class "open" from the overlay
-closeModalBtn.addEventListener('click', () => {
-    closeModal();
-})
+closeModalBtn.addEventListener('click', closeModal);
 // BONUS: Add a click event listener to the overlay that removes the class "open" from the modal and the overlay
-overlay.addEventListener('click', () => {
-    closeModal();
-})
+overlay.addEventListener('click', closeModal);
 function closeModal() {
     modal.classList.remove('open');
     overlay.classList.remove('open');
+}
+function openModal() {
+    modal.classList.add('open');
+    overlay.classList.add('open');
 }
