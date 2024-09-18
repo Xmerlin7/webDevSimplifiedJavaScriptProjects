4cd227b3256c72bca28a4038f130bdba41598d18
Seif Allah <saifeldarageely@gmail.com>
2024-09-18 17:08:16 +0300

    Refactor index.html and script.js to update HTML elements' styles and add nested divs

--- setup/script.js
+++ setup/script.js
@@ -1,7 +1,10 @@
-const test = document.querySelectorAll('button')
-test.forEach((button) => {
-    button.addEventListener('click', () => {
-        const currentClicks = parseInt(button.dataset.clicks);
-        button.dataset.clicks = currentClicks + 1;
-    });
-});
+const grandParent = document.querySelector('#grandparent');
+// const parent = document.querySelector('#parent');
+const child = document.querySelector('#child');
+grandParent.style.color = 'red';
+const parent1 = grandParent.children[0]
+const parent2 = grandParent.children[1]
+
+parent1.style.color = 'blue';
+parent2.children[0].style.color = 'blue';
+
