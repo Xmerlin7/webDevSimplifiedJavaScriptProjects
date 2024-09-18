d160e508591d64ecf3bd41944a138afd82be8118
Seif Allah <saifeldarageely@gmail.com>
2024-09-18 19:50:04 +0300

    Refactor script.js to add form for adding tasks

--- toDoList/script.js
+++ toDoList/script.js
@@ -1,3 +1,5 @@
 //! Select All Elements
-
+const form = document.querySelector('#new-item-form')
+const input = document.querySelector('#item-input')
+const btn = document,querySelector('button')
 //! When the user submit the form add the task to the list
