66f49fc54f684e72a44e31f5eb9423f9837a58a0
Seif Allah <saifeldarageely@gmail.com>
2024-09-21 01:04:15 +0300

    Refactor script.js to handle promise rejections in setTimePromise function

--- 0x00-setup/script.js
+++ 0x00-setup/script.js
@@ -1,11 +1,24 @@
 function setTimePromise(time) {
     return new Promise((resolve, reject) => {
-        setTimeout(resolve(`Waited for ${time} ms`)
+        setTimeout(reject(`Waited for ${time} ms`)
+            , time);
+    })
+}
+
+function setTimePromise(time) {
+    return new Promise((resolve, reject) => {
+        setTimeout(reject(`Waited for ${time} ms`)
             , time);
     })
 }
 async function setTime(time) {
-    const message = await setTimePromise(time);
-    console.log(message);
+    try {
+        let message = await setTimePromise(time);
+        console.log(message);
+        message = await setTimePromise(time);
+        console.log(message);
+    }catch (error) {
+        console.error(error);
+    }
 }
 setTime(200);
