0451f9fca498ab4fecfa7d8180690fd1605baf9d
Seif Allah <saifeldarageely@gmail.com>
2024-09-20 21:52:16 +0300

    Refactor script.js to remove unnecessary code and add a delayed console log message

--- 0x00-setup/script.js
+++ 0x00-setup/script.js
@@ -6,24 +6,6 @@ setTimeout(() => {
             console.log(3)
             setTimeout(() => {
                 console.log(4)
-                setTimeout(() => {
-                    console.log(5)
-                    setTimeout(() => {
-                        console.log(6)
-                        setTimeout(() => {
-                            console.log(7)
-                            setTimeout(() => {
-                                console.log(8)
-                                setTimeout(() => {
-                                    console.log(9)
-                                    setTimeout(() => {
-                                        console.log(10)
-                                    }, 1000);
-                                }, 1000);
-                            }, 1000);
-                        }, 1000);
-                    }, 1000);
-                }, 1000);
             }, 1000);
         }, 1000);
     }, 1000);
