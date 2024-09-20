0b8acd39fc5a738e335b9e86de7ca48cb76c934a
Seif Allah <saifeldarageely@gmail.com>
2024-09-20 20:18:28 +0300

    Refactor script.js to remove unnecessary code and add a delayed console log message

--- 0x00-setup/script.js
+++ 0x00-setup/script.js
@@ -1,19 +1,3 @@
-let person = {
-    name: 'John',
-    friend: {
-        name: 'Chris',
-        friend: {
-            name: 'Matt'
-        }
-    }
-}
-let per = person
-function printFriends(pers) {
-    if (pers == null) {
-        return
-    }
-    console.log(pers.name)
-    pers = pers.friend
-    printFriends(pers)
-}
-printFriends(per)
+setTimeout(() => {
+    console.log('Hello from script.js');
+}, 1000);
