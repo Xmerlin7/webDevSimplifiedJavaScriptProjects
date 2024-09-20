be076034ed8ae502cc775f1f397b66baf1e2f3c0
Seif Allah <saifeldarageely@gmail.com>
2024-09-20 23:14:57 +0300

    Refactor script.js to add chained promise handling for button click and mouseover events

--- 0x00-setup/script.js
+++ 0x00-setup/script.js
@@ -4,17 +4,19 @@
 
 // I will write the code to add the event listener to the button
 const button = document.querySelector('button');
+function addEventListenerPromise(element, event) {
+    return new Promise((resolve, reject) => {
+        element.addEventListener(event, resolve);
+    })
+}
 
-addEventListenerPromise(button, 'click')
+while (true) {
+    addEventListenerPromise(button, 'click')
     .then(() => {
         console.log('Button was clicked');
         return addEventListenerPromise(button, 'mouseover');
     }).then(() => {
         console.log('Button was hovered');
     });
-
-function addEventListenerPromise(element, event) {
-    return new Promise((resolve, reject) => {
-        element.addEventListener(event, resolve);
-    })
 }
+
