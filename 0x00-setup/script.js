e3f66162656e79249fecf7d374d9d9f315302075
Seif Allah <saifeldarageely@gmail.com>
2024-09-20 23:31:21 +0300

    Refactor script.js to remove commented out code and add chained promise handling for console.log messages

--- 0x00-setup/script.js
+++ 0x00-setup/script.js
@@ -1,22 +1,2 @@
-// copilot please don't help me in this file
-// I want to write this file by myself
-// I will ask for your help when I need it
-
-// I will write the code to add the event listener to the button
-const button = document.querySelector('button');
-function addEventListenerPromise(element, event) {
-    return new Promise((resolve, reject) => {
-        element.addEventListener(event, resolve);
-    })
-}
-
-while (true) {
-    addEventListenerPromise(button, 'click')
-    .then(() => {
-        console.log('Button was clicked');
-        return addEventListenerPromise(button, 'mouseover');
-    }).then(() => {
-        console.log('Button was hovered');
-    });
-}
-
+let fifteen = Promise.resolve(15);
+fifteen.then(value => console.log(`Got ${value}`));
