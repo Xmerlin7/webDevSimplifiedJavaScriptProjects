e4a0ee51d094cf35e197bb5a54d25c03bcb27fc3
Seif Allah <saifeldarageely@gmail.com>
2024-09-21 04:10:45 +0300

    Add node-fetch dependency

--- 0x00-setup/script.js
+++ 0x00-setup/script.js
@@ -1,32 +1,3 @@
-function setTimePromise(time) {
-    return new Promise((resolve, reject) => {
-        setTimeout(resolve(`Waited for ${time} ms`)
-            , time);
-    })
-}
-
-function setTimePromiseError(time) {
-    return new Promise((resolve, reject) => {
-        setTimeout(reject(`Waited for ${time} ms`)
-            , time);
-    })
-}
-
-async function setTime(time) {
-    try {
-        console.log('Start before error');
-        let message = await setTimePromise(time);
-        console.log(message);
-        message = await setTimePromiseError(time);
-        console.log(message);
-        console.log('End after error');
-        message = await setTimePromise(time);
-        console.log(message);
-    }catch (error) {
-        console.log(' hehe error');
-        console.error(error);
-    }finally {
-        console.log('أنا محدش يقدر يوقفني');
-    }
-}
-setTime(200);
+fetch('https://jsonplaceholder.typicode.com/todos/1')
+    .then(response => response.json())
+    .then(data => console.log(data))
