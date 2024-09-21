11e66b3a985c45886e88694087c3e0e117260f93
Seif Allah <saifeldarageely@gmail.com>
2024-09-21 17:02:57 +0300

    Refactor script.js to remove unused code and add test function

--- 0x00-setup/script.js
+++ 0x00-setup/script.js
@@ -1,5 +1,4 @@
-fetch('https://jsonplaceholder.typicode.com/users')
-    .then(response => response.json())
-    .then(data => data.map(user => {
-        console.log(user.username)
-    }));
+function test() {
+
+}
+test()
