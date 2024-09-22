2a464b47403cafd8a0534ae1b0a3fab00c742e42
Seif Allah <saifeldarageely@gmail.com>
2024-09-22 16:04:47 +0300

    Refactor index.html and script.js to add descriptive button IDs

--- 0x00-setup/script.js
+++ 0x00-setup/script.js
@@ -1,12 +1,4 @@
-
-document.addEventListener('click', e => {
-    e.preventDefault()
-    if (e.target.id === '1')
-        console.log('Button 1 clicked')
-    else if (e.target.id === '2')   
-        console.log('Button 2 clicked')
-    else if (e.target.id === '3')
-        console.log('Button 3 clicked')
-    else if (e.target.id === '4')
-        console.log('Button 4 clicked')
+document.addEventListener('click',  e => {
+    if(e.target.matches('#btn1'))
+        console.log('button 1 clicked');
 })
