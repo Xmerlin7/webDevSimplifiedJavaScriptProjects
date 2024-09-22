3a96a84cd07edb435bd587ad52671fdf003c1300
Seif Allah <saifeldarageely@gmail.com>
2024-09-22 15:14:19 +0300

    Refactor index.html and script.js to add multiple buttons and handle click events

--- 0x00-setup/script.js
+++ 0x00-setup/script.js
@@ -1,7 +1,12 @@
 
-document.addEventListener('click', function (e) {
-    if (e.target.id === '1') {
-        e.preventDefault(); // prevent the default action
-        console.log('Link clicked');
-    }});
-
+document.addEventListener('click', e => {
+    e.preventDefault()
+    if (e.target.id === '1')
+        console.log('Button 1 clicked')
+    else if (e.target.id === '2')   
+        console.log('Button 2 clicked')
+    else if (e.target.id === '3')
+        console.log('Button 3 clicked')
+    else if (e.target.id === '4')
+        console.log('Button 4 clicked')
+})
