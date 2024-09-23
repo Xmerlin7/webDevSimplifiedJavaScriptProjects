7f8e230c3894397f20143566ff18ca74b451788f
Seif Allah <saifeldarageely@gmail.com>
2024-09-23 07:12:46 +0300

    Refactor script.js to fix missing newline at end of file

--- 0x05-Advanced-TODO/script.js
+++ 0x05-Advanced-TODO/script.js
@@ -3,14 +3,21 @@ const todoList = document.querySelector('#list')
 const todoInput = document.querySelector('#todo-input')
 const todotemplate = document.querySelector('#list-item-template')
 const todoCheckBox = document.querySelector('[data-list-item-checkbox]')
-
+let todos = []
 todoForm.addEventListener('submit', e => {
     e.preventDefault()
     const todoText = todoInput.value
-    console.log(todoText)
-    renderTodo(todoText)
     todoInput.value = ''
+    renderTodo(todoText)
+    saveTodo()
+
 })
-function renderTodo(){
-    // No operation
+function renderTodo(todoName){
+    const templateClone = todotemplate.content.cloneNode(true)
+    const textSpan = templateClone.querySelector('[data-list-item-text]')
+    textSpan.innerText = todoName
+    todoList.appendChild(templateClone)
+}
+function saveTodo(){
+    // Empty
 }
