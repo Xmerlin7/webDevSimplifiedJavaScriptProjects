10390147a05a16a87ab83aa9e570bfee19046902
Seif Allah <saifeldarageely@gmail.com>
2024-09-23 06:45:07 +0300

    Refactor index.html and script.js to fix file references and add basic structure for an advanced todo list

--- 0x05-Advanced-TODO/script.js
+++ 0x05-Advanced-TODO/script.js
@@ -1,1 +1,16 @@
-const form = document.querySelector('#new-todo-form')
+const todoForm = document.querySelector('#new-todo-form')
+const todoList = document.querySelector('#list')
+const todoInput = document.querySelector('#todo-input')
+const todotemplate = document.querySelector('#list-item-template')
+const todoCheckBox = document.querySelector('[data-list-item-checkbox]')
+
+todoForm.addEventListener('submit', e => {
+    e.preventDefault()
+    const todoText = todoInput.value
+    console.log(todoText)
+    renderTodo(todoText)
+    todoInput.value = ''
+})
+function renderTodo(){
+    // No operation
+}
