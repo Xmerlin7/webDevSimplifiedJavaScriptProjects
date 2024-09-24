a518c87f85cf1f47040bc92d9b4b03a07016822e
Seif Allah <saifeldarageely@gmail.com>
2024-09-24 17:50:11 +0300

    Refactor script.js and fix typo in index.html

--- 0x08-DatePicker/script.js
+++ 0x08-DatePicker/script.js
@@ -1,7 +1,11 @@
-console.log('hi')
+import { format } from 'date-fns';
 const dateBtn = document.querySelector('.date-picker-button')
 const dateCard = document.querySelector('.date-picker')
 dateBtn.addEventListener('click', e => {
     e.preventDefault()
     dateCard.classList.toggle('show')
+    const today = new Date();
+    console.log(today)
+    console.log(format(today, 'yyyy-MM-dd'))
+    dateBtn.innerText = format(today, 'yyyy-MM-dd')
 })
