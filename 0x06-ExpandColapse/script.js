312cad9820b6a40fe1ed664ca798dc474560fd47
Seif Allah <saifeldarageely@gmail.com>
2024-09-24 09:38:39 +0300

    Refactor index.html to fix expand/collapse functionality and missing newline at end of file

--- 0x06-ExpandColapse/script.js
+++ 0x06-ExpandColapse/script.js
@@ -3,9 +3,18 @@ document.addEventListener('click', e => {
     //get the clicked button
     if(!e.target.matches('.expand-button')) return
     // get the id of the clicked button
+    //add show
+    showCard(e)
+    //remove show
+    removeCard(e)
+    //toggle button
+    toggleCard(e)
+
+})
+function showCard(e){
     const hitId = e.target.dataset.id
     // next class
     const nextSibling = e.target.parentElement.nextElementSibling;
+    nextSibling.classList.add('show'); 
     console.log(nextSibling)
-    //add show
-})
+}
