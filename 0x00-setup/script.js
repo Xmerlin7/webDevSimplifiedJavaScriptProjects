8850e6307c648aa3dd1a67bef3c5ee111649c0de
Seif Allah <saifeldarageely@gmail.com>
2024-09-28 18:46:44 +0300

    Refactor script.js to destructure object properties

--- 0x00-setup/script.js
+++ 0x00-setup/script.js
@@ -2,11 +2,12 @@ const arr = ['A', 'B', 'C', 'D']
 const obj = {
     name: "Mohy",
     age: 22,
-    address : {
+    address: {
         street: "kafr elshigk",
         id: 9
     }
 }
-const {name : sa7by, age, address: {street}} = obj
+const { name: sa7by, age, address: { street } } = obj
 console.log(sa7by)
+
 console.log(street)
